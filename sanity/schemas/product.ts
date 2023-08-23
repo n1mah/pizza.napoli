dfd9bd185e1b71d712fc2175de78767c022e4b5d
n1mah <nima.heidari.n@gmail.com>
2023-08-23 20:05:52 +0330

    product schema added

---
+++ sanity/schemas/product.ts
@@ -0,0 +1,37 @@
+export default {
+    name: 'product',
+    type: 'document',
+    title: 'product',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name'
+        },
+        {
+            name: 'price',
+            type: 'integer',
+            title: 'price'
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'slug'
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Image'
+        },
+        {
+            name: 'details',
+            type: 'array',
+            title: 'Details',
+            of:[
+                {
+                    type: "block"
+                },
+            ]
+        }
+    ]
+}
