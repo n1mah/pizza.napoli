eb92f2430fc6b887cf6b855a6d025c01b10a5d7e
n1mah <nima.heidari.n@gmail.com>
2023-08-23 20:36:50 +0330

    get data from sanity - added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Navbar from "@/app/components/navbar";
+import {client} from "@/app/lib/sanity";
 
-export default function Home() {
+async function getData(){
+    const query=`*[_type == "product"]`;
+    return await client.fetch(query);
+}
+export default async function Home() {
+    const data = await getData();
   return (
     <div className="w-8/12 mx-auto">
         <Navbar/>
@@ -12,6 +18,7 @@ export default function Home() {
           <p className=' px-4 py-2 mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto asperiores autem, consequatur cum cupiditate dolores dolorum eaque eum hic inventore sapiente, totam unde vitae voluptatum? Laboriosam libero necessitatibus quisquam!</p>
           <p className=' px-4 py-2 mt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto asperiores autem, consequatur cum cupiditate dolores dolorum eaque eum hic inventore sapiente, totam unde vitae voluptatum? Laboriosam libero necessitatibus quisquam!</p>
           <div className='flex '>
+
 
           </div>
       </div>
